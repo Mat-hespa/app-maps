9d315223d8d4a226c251c0f6b3426acda11ed1ca
Matheus Hespagnola <matheushespa@outlook.com>
2025-08-08 19:35:08 -0300

    reconfig PWA

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ bootstrapApplication(App, appConfig)
 // Registro do Service Worker
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js')
+    navigator.serviceWorker.register('/assets/sw.js')
       .then((registration) => {
         console.log('SW registered: ', registration);
       })
